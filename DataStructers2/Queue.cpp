da014017c9affb73af779cf72e1ad9a433db1383
pp486 <polinaperelman.il@gmail.com>
2024-11-11 10:08:06 +0200

    DataStructers2

--- DataStructers2/Queue.cpp
+++ DataStructers2/Queue.cpp
@@ -3,14 +3,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void initQueue(Queue* q, unsigned int size)
+void initQueue(Queue* q, unsigned int size) 
 {
     q->elements = (unsigned int*)malloc(size * sizeof(unsigned int));
+    q->size = size;
     q->head = 0;
     q->tail = 0;
     q->currentSize = 0;
 }
-void cleanQueue(Queue* q) 
+
+void cleanQueue(Queue* q)
 {
     free(q->elements);
     q->elements = NULL;
@@ -18,6 +20,7 @@ void cleanQueue(Queue* q)
     q->tail = 0;
     q->currentSize = 0;
 }
+
 void enqueue(Queue* q, unsigned int newValue)
 {
     if (isFull(q)) 
@@ -26,27 +29,29 @@ void enqueue(Queue* q, unsigned int newValue)
         return;
     }
     q->elements[q->tail] = newValue;
-    q->tail = (q->tail + 1) % q->size; 
+    q->tail = (q->tail + 1) % q->size;
     q->currentSize++;
 }
-int dequeue(Queue* q)
+
+int dequeue(Queue* q) 
 {
     if (isEmpty(q)) 
     {
         printf("Queue is empty, cannot dequeue\n");
-        return -1; 
+        return -1;
     }
     int value = q->elements[q->head];
-    q->head = (q->head + 1) % q->size; 
+    q->head = (q->head + 1) % q->size;
     q->currentSize--;
     return value;
 }
-bool isEmpty(Queue* s) 
+
+bool isEmpty(Queue* s)
 {
     return s->currentSize == 0;
 }
-bool isFull(Queue* s) 
+
+bool isFull(Queue* s)
 {
     return s->currentSize == s->size;
 }
-
--- DataStructers2/Queue.h
+++ DataStructers2/Queue.h
@@ -1,24 +1,22 @@
+#pragma once
 #ifndef QUEUE_H
 #define QUEUE_H
 
 #include <stdbool.h>
-/* a queue contains positive integer values. */
+
 typedef struct Queue
 {
-    unsigned int* elements;     
-    unsigned int size;          
-    unsigned int head;         
-    unsigned int tail;        
+    unsigned int* elements;
+    unsigned int size;
+    unsigned int head;
+    unsigned int tail;
     unsigned int currentSize;
-    unsigned int newValue;
 } Queue;
 
 void initQueue(Queue* q, unsigned int size);
 void cleanQueue(Queue* q);
-
 void enqueue(Queue* q, unsigned int newValue);
-int dequeue(Queue* q); // return element in top of queue, or -1 if empty
-
+int dequeue(Queue* q);
 bool isEmpty(Queue* s);
 bool isFull(Queue* s);
 
