9f3b98d3b98fc3168bffe50ed9e6a1e2265ca7cd
pp486 <polinaperelman.il@gmail.com>
2024-11-11 10:29:48 +0200

    k

--- DataStructers2/LinkedList.cpp
+++ DataStructers2/LinkedList.cpp
@@ -5,3 +5,20 @@ void initList(LinkedList* list)
 {
     list->head = nullptr;
 }
+void addToFront(LinkedList* list, int value) 
+{
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->next = list->head;
+    list->head = newNode;
+}
+void removeFromFront(LinkedList* list) 
+{
+    if (list->head == nullptr) {
+        std::cout << "The list is empty, cannot remove element." << std::endl;
+        return;
+    }
+    Node* temp = list->head;
+    list->head = list->head->next;
+    delete temp;
+}
